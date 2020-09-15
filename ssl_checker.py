1b12946f345ffec4ab1b7a9d769060888092e988
Pedro Oliveira <50972339+pedrohso90@users.noreply.github.com>
2020-09-14 22:37:25 -0300

    Create ssl_checker.py

---
+++ ssl_checker.py
@@ -0,0 +1,14 @@
+from urllib.request import Request, urlopen, ssl, socket
+from urllib.error import URLError, HTTPError
+import json
+#some site without http/https in the path
+base_url = 'www.google.com.br'
+port = '443'
+hostname = base_url
+context = ssl.create_default_context()
+with socket.create_connection((hostname, port)) as sock:
+    with context.wrap_socket(sock, server_hostname=hostname) as ssock:
+        print(ssock.version())
+        data = json.dumps(ssock.getpeercert())
+        # print(ssock.getpeercert())
+print (data)
